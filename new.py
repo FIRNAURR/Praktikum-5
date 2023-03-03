0e38c4204eb0e53a48b6a6fcde59d44759faa0bb
Riskaku <ulfianifirna@gmail.com>
2023-03-04 01:14:33 +0700

    1

--- new.py
+++ new.py
@@ -39,3 +39,10 @@ hasil1.cetakluas()
 print(hasil1)
 hasil1.cetakkeliling()
 print(hasil1)
+
+stk = [1,2,3,4,5]
+print(stk)
+#memasukkan data baru
+stk.append(11)
+print("data masuk", 11)
+print("data sekarang", stk)
